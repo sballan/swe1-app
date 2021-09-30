4d5044c224f0623b5fa088a4a712f6f8418ffb06
Samuel Ballan <sgb4622@gmail.com>
2021-09-30 15:43:53 -0400

    silly heroku

--- django_helloworld/settings.py
+++ django_helloworld/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Special Heroku settings hook
+django_heroku.settings(locals())
