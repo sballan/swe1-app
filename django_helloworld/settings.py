afa920bcfe9eaa01ceabfe4b42cc6be84d10d3ad
Samuel Ballan <sgb4622@gmail.com>
2021-10-22 20:54:01 -0400

    test travis

--- django_helloworld/settings.py
+++ django_helloworld/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
+travis_works = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation."
@@ -93,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
+
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
